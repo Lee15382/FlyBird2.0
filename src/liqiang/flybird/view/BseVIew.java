26d3d375934d7448c198d7a80a57871c06eed010
Lee15382 <983436190@qq.com>
2016-04-26 15:31:30 +0800

    first commit

---
+++ src/liqiang/flybird/view/BseVIew.java
@@ -0,0 +1,13 @@
+package liqiang.flybird.view;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class BseVIew extends SurfaceView {
+
+	public BseVIew(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
